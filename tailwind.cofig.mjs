1f6cf9bcbaef8be6368066e49e8bbffc8164062e
Surya A <suryasu5678@gmail.com>
2025-03-11 23:07:20 +0530

    AiSolutionPage

--- tailwind.cofig.mjs
+++ tailwind.cofig.mjs
@@ -11,7 +11,7 @@ export default {
 				md: '768px',  // Custom medium container size
 				lg: '1024px',  // Custom large container size
 				xl: '1280px', // Custom extra-large container size
-				'2xl': '1536px' // Custom 2xl container size
+				'2xl': '1736px' // Custom 2xl container size
 			}
 		}
 	},
